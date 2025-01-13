f40a8ad41141ff3cb22b672ffb4f2af0ac21b8b7
olamide-ilori <olailori01@gmail.com>
2025-01-12 22:05:03 -0500

    Working with object types and fields

---
+++ data/schema.js
@@ -0,0 +1,20 @@
+//blueprint of data that GraphQL will be accepting
+
+import { buildSchema } from "graphql";
+
+const schema = buildSchema(`
+
+    type Product {
+        id: ID
+        name: String
+        description: String
+        price: Float
+        soldout: Boolean
+    
+    }
+        type Query {
+            product: Product
+        }
+    `);
+
+export default schema;
---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import schema from './data/schema';
+
+const PORT = 8080;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GraphQL is amazing!');
+});
+
+//const root = {hello: () => "Hello, I am Alfie"} //resolver
+const root = {
+    product: () => {
+        return {
+            "id": 36646272772,
+            "name": "Car",
+            "description": "2020 Toyota Hybrid",
+            "price": 30000.00,
+            "soldout": false
+        }
+    }};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+
+app.listen(PORT, ( ) => console.log(`Running server on localhost:${PORT}/graphql`));
+
